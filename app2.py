27f1f5beb5816999c9ebe2793845592a1b13858c
RayyanFikry <147078770+RayyanFikry@users.noreply.github.com>
2025-04-22 16:06:59 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,18 +6,43 @@ st.title('Loan Prediction App')
 
 model = load('best_rf_model.pkl')
 
-def predict_loan_status(features):
-    encoded_features = [
-        categorical_features['person_gender'][features[1]], 
-        categorical_features['person_education'][features[2]],  
-        categorical_features['person_home_ownership'][features[5]], 
-        categorical_features['loan_intent'][features[7]]
-    ] + features[0:1] + features[3:5] + features[6:8] + features[9:]
+categorical_features = {
+    'person_gender': {'Male': 1, 'Female': 0},
+    'person_education': {'High School': 0, 'Bachelors': 1, 'Masters': 2, 'PhD': 3},
+    'person_home_ownership': {'Own': 0, 'Rent': 1, 'Mortgage': 2},
+    'loan_intent': {'Personal': 0, 'Business': 1, 'Debt Consolidation': 2}
+}
 
-    features_df = pd.DataFrame([encoded_features])
+def predict_loan_status(features):
+    # Susun urutan fitur sesuai training
+    encoded_features = [
+        features[0],  # person_age
+        categorical_features['person_gender'][features[1]],
+        categorical_features['person_education'][features[2]],
+        features[3],  # person_income
+        features[4],  # person_emp_exp
+        categorical_features['person_home_ownership'][features[5]],
+        features[6],  # loan_amnt
+        categorical_features['loan_intent'][features[7]],
+        features[8],  # loan_int_rate
+        features[9],  # loan_percent_income
+        features[10], # cb_person_cred_hist_length
+        features[11], # credit_score
+        features[12], # previous_loan_defaults_on_file
+    ]
+
+    # Pastikan kolom sesuai dengan urutan dan jumlah fitur saat training
+    columns = [
+        'person_age', 'person_gender', 'person_education', 'person_income', 'person_emp_exp',
+        'person_home_ownership', 'loan_amnt', 'loan_intent', 'loan_int_rate', 'loan_percent_income',
+        'cb_person_cred_hist_length', 'credit_score', 'previous_loan_defaults_on_file'
+    ]
+    
+    features_df = pd.DataFrame([encoded_features], columns=columns)
     prediction = model.predict(features_df)
     return prediction[0]
 
+# Input form
 person_age = st.number_input('Age of the Person', min_value=18, max_value=100, step=1)
 person_gender = st.selectbox('Gender of the Person', ['Male', 'Female'])
 person_education = st.selectbox('Education Level', ['High School', 'Bachelors', 'Masters', 'PhD'])
@@ -47,13 +72,6 @@ input_features = [
     credit_score,
     previous_loan_defaults_on_file
 ]
-
-categorical_features = {
-    'person_gender': {'Male': 1, 'Female': 0},
-    'person_education': {'High School': 0, 'Bachelors': 1, 'Masters': 2, 'PhD': 3},
-    'person_home_ownership': {'Own': 0, 'Rent': 1, 'Mortgage': 2},
-    'loan_intent': {'Personal': 0, 'Business': 1, 'Debt Consolidation': 2}
-}
 
 if st.button('Predict Loan Status'):
     prediction = predict_loan_status(input_features)
